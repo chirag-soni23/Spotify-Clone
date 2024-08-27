c5d4f2ab5c682a6cd4a148c0ffa9abd8bf02be21
Chirag Soni <csoni0693@gmail.com>
2024-08-27 07:46:24 +0530

    spotify

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='flex items-center justify-center min-h-screen'>
-      <div className="bg-red-500 text-white p-8 rounded-lg shadow-lg max-w-md w-full">
+      <div className="bg-black text-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className='text-3xl font-semibold text-center mb-8'>Login to Spotify</h2>        
         </div>      
     
