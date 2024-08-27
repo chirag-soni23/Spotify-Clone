d513a968da4528fee4fc00a1492e486ab0eff04f
Chirag Soni <csoni0693@gmail.com>
2024-08-27 10:40:08 +0530

    spotify

--- Backend/controllers/songController.js
+++ Backend/controllers/songController.js
@@ -1,5 +1,20 @@
+import { Album } from "../models/Album";
 import TryCatch from "../utils/TryCatch";
+import getDataurl from "../utils/urlGenerator";
+import cloudinary from 'cloudinary';
 
 export const createAlbum = TryCatch(async(req,res)=>{
+    if(req.user.role !== "admin") return res.status(403).json({message:"You are not admin"});
+    const {title,description} = req.body
+    const file = req.file;
+    const fileUrl = getDataurl(file);
+    const cloud = await cloudinary.v2.uploader.upload(fileUrl.content);
+    await Album({
+        title,description,thumbnail:{
+            id:cloud.public_id,
+            url:cloud.secure_url
+        }
+    });
+    res.json({message:"Album Added successfull!"});
     
 })
