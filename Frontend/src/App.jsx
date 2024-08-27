4c5c1ef7bfe4ec574e47df216dd8415ce420d0c5
Chirag Soni <csoni0693@gmail.com>
2024-08-27 08:16:14 +0530

    spotify

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './pages/Login'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
       <Route path='/login' element={<Login/>}>
       </Route>
     </Routes>
