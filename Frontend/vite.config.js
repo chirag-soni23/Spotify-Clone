8174bf16ef85378b713fe141b2929df7a91208ba
Chirag Soni <csoni0693@gmail.com>
2024-12-04 17:52:57 +0530

    spotify

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:"http://localhost:5000",
+        target:"https://spotify-lpnu.onrender.com/",
       }
     }
   }
